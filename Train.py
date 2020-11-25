b9088be694f43d13f8c667df6dd7cd5037c82909
zzzxxx00019 <71003018+zzzxxx00019@users.noreply.github.com>
2020-11-26 00:59:53 +0800

    Modify model parameter

--- Train.py
+++ Train.py
@@ -10,16 +10,8 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 import matplotlib.pyplot as plt
 
-def Read_Data():
-    Barcelona = pd.read_csv('./city_var2/Barcelona.csv').drop(columns=['Unnamed: 0', 'city_name'])
-    Bilbao = pd.read_csv('./city_var2/Bilbao.csv').drop(columns=['Unnamed: 0', 'city_name'])
-    Madrid = pd.read_csv('./city_var2/Madrid.csv').drop(columns=['Unnamed: 0', 'city_name'])
-    Seville = pd.read_csv('./city_var2/Seville.csv').drop(columns=['Unnamed: 0', 'city_name'])
-    Valencia = pd.read_csv('./city_var2/Valencia.csv').drop(columns=['Unnamed: 0', 'city_name'])
-    return Barcelona, Bilbao, Madrid, Seville, Valencia
-
 def Load_Energy_Data():
-    Dataset = pd.read_csv('./energy_dataset.csv').drop(columns=['time', 'forecast wind offshore eday ahead'])
+    Dataset = pd.read_csv('./energy_dataset.csv')
     Dataset = Dataset.iloc[:]['price actual']
     return Dataset
 
@@ -29,28 +21,28 @@ def normalize(train):
 
 def LSTM_Model(input_shape):
     model = Sequential()
-    model.add(LSTM(256, input_length = input_shape[1], input_dim=input_shape[2], return_sequences=True))
+    model.add(LSTM(128, input_length = input_shape[1], input_dim=input_shape[2], return_sequences=True))
+    model.add(LSTM(256, return_sequences=True))
     model.add(Dropout(0.1))
     model.add(LSTM(256, return_sequences=True))
-    model.add(Dropout(0.1))    
-    model.add(TimeDistributed(Dense(1)))
+    model.add(Dropout(0.2))
     model.add(Flatten())
-    model.add(Dense(20,activation='linear'))
-    model.add(Dense(10,activation='linear'))
+    model.add(Dense(25))
+    model.add(Dense(5,activation='linear'))
     model.add(Dense(1))
-    model.compile(loss="mean_absolute_error", optimizer="Adam",metrics=['mean_absolute_error'])
+    model.compile(loss="mean_absolute_error", optimizer="Adam",metrics=['mape'])
     model.summary()
     return model
 
 def Draw(history):
     fig = plt.figure()
-    plt.plot(history.history['loss'],label='training loss')
-    plt.plot(history.history['val_loss'], label='val loss')
+    plt.plot(history.history['loss'], 'ro',label='training loss', color='blue')
+    plt.plot(history.history['val_loss'], 'ro', label='val loss', color='red')
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(loc='upper right')
-    fig.savefig('loss.png')
+    fig.savefig('./result_image/loss.png')
 
 def BuildTrain(data, past, future):
     X_train, Y_train = [], []
@@ -73,20 +65,13 @@ def PricePredict(predicted_data, actual_result, slide, fig_name):
     plt.xlabel('Time')
     plt.ylabel('Price')    
     plt.legend()
-    fig.savefig(fig_name)
+    fig.savefig('./result_image/'+fig_name)
         
 def Training_Model(x, y):
-    x_train, x_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2)
-
-    print(x_train.shape)
-    print(Y_train.shape)
-    
-    print(x_test.shape)
-    print(Y_test.shape)
+    x_train, x_test, Y_train, Y_test = train_test_split(x, y, test_size=0.25)
     
     model = LSTM_Model(x_train.shape)
-    #callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-    history = model.fit(x_train, Y_train, epochs=800, batch_size=64, validation_data=(x_test, Y_test))
+    history = model.fit(x_train, Y_train, epochs=500, batch_size=64, validation_data=(x_test, Y_test))
     Draw(history)
 
     model.save('LSTM.h')
@@ -95,13 +80,9 @@ def Training_Model(x, y):
     PricePredict(model.predict(x_test) ,Y_test, 50, 'validation_Predict')
 
 if __name__ == '__main__' :
-    #Barcelona, Bilbao, Madrid, Seville, Valencia = Read_Data()
-    Weather = pd.read_csv('MergeTable.csv').drop(columns=['Unnamed: 0'])
+    Weather = pd.read_csv('weather_dataset.csv').drop(columns=['Unnamed: 0'])
     Energy = Load_Energy_Data()
-
     data = pd.concat ( [Weather, Energy], axis=1 )
-    
-    print(data)
     
     x_train, y_train = BuildTrain(data, 20, 1)
     Training_Model(x_train, y_train)
